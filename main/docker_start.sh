92f2a7ed5011dcd605dd57a1fad0a63267526eaf
Zelda <Zelda@Zelda.com>
2024-05-07 15:57:56 -0400

    Docker start script

--- main/docker_start.sh
+++ main/docker_start.sh
@@ -1,7 +1,7 @@
 # Update api keys
-RUN sed -i -r 's/censys_api_id: ""/censys_api_id: \"'$CENSYS_API_ID'\"/g' ../api.yaml
-RUN sed -i -r 's/censys_secret: ""/censys_secret: \"'$CENSYS_SECRET'\"/g' ../api.yaml
-RUN sed -i -r 's/shodan: ""/shodan: \"'$SHODQN'\"/g' ../api.yaml
-RUN sed -i -r 's/huntio: ""/huntio: \"'$HUNTIO'\"/g' ../api.yaml
+sed -i -r 's/censys_api_id: ""/censys_api_id: \"'$CENSYS_API_ID'\"/g' ../api.yaml
+sed -i -r 's/censys_secret: ""/censys_secret: \"'$CENSYS_SECRET'\"/g' ../api.yaml
+sed -i -r 's/shodan: ""/shodan: \"'$SHODQN'\"/g' ../api.yaml
+sed -i -r 's/huntio: ""/huntio: \"'$HUNTIO'\"/g' ../api.yaml
 
 ./NoWhere2Hide
